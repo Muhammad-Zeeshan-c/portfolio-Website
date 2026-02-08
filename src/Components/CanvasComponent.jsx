c78859d99489027f73e0174e381afdddc8922cc9
Muhammad Zeeshan <m.zeeshankhalid12@gmail.com>
2026-02-08 22:45:14 +0500

    particles background added

---
+++ src/Components/CanvasComponent.jsx
@@ -0,0 +1,69 @@
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim"; 
+
+const ParticlesBackground = () => {
+  const [init, setInit] = useState(false);
+
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+
+  const options = useMemo(() => ({
+    fullScreen: { enable: true, zIndex: -1 }, 
+    
+    background: {
+      color: { value: "#000000" },
+    },
+    fpsLimit: 120,
+    interactivity: {
+      events: {
+        onHover: {
+          enable: true,
+          mode: "grab", 
+        },
+      },
+      modes: {
+        grab: { distance: 150, links: { opacity: 0.5 } },
+      },
+    },
+    particles: {
+      color: { value: "#ffffff" },
+      links: {
+        color: "#ffffff",
+        distance: 150,
+        enable: true,
+        opacity: 0.3,
+        width: 1,
+      },
+      move: {
+        enable: true,
+        speed: 1.5,
+        direction: "none",
+        outModes: { default: "out" },
+      },
+      number: {
+        value: 100, 
+        density: { enable: true, area: 800 },
+      },
+      opacity: { value: 0.5 },
+      shape: { type: "circle" },
+      size: { value: { min: 1, max: 3 } },
+    },
+    detectRetina: true,
+  }), []);
+
+  if (init) {
+    return <Particles id="tsparticles" options={options} />;
+  }
+
+  return null;
+};
+
+export default ParticlesBackground;
