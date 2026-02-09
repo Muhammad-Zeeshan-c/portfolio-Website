e75fc536f00e9e12c85a84052b3c6df3350c5c70
Muhammad Zeeshan <m.zeeshankhalid12@gmail.com>
2026-02-09 11:01:51 +0500

    custom cursor added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import ParticlesBackground from "./Components/CanvasComponent.jsx"
+import CursorComponent from "./Components/CursorComponent.jsx"
+
+
+
+
 function App() {
 
 
   return (
     <>
       <ParticlesBackground />
+      <CursorComponent />
     </>
   )
 }
---
+++ src/Components/CursorComponent.jsx
@@ -0,0 +1,27 @@
+import React,{useEffect, useState}from 'react'
+
+function CursorComponent() {
+
+  const [position,setposition]=useState({x:0,y:0});
+
+  const handleMove=(e)=>{
+    setposition({x:e.clientX,y:e.clientY});
+  }
+
+  useEffect(()=>{
+    window.addEventListener('mousemove',handleMove);
+    return()=>{
+      window.removeEventListener('mousemove',handleMove);
+    }
+  })
+  return (
+    <div className='pointer-events-none z-[999] fixed top-0 left-0'
+      style={{transform:`translate(${position.x}px , ${position.y}px )`}}>
+      <div className='w-20 h-20 bg-gradient-to-r bg-pink-500 to-blue-500 blur-3xl opacity-70'>
+
+      </div>
+    </div>
+  )
+}
+
+export default CursorComponent
